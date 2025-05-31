718af9010b982ec0be2c7545a336b004cd9511f8
danbart <danilo_solorzano06@hotmail.com>
2025-05-31 17:59:37 -0600

    rutas desentralizadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const casosRoutes = require('./routes/casos.routes');
-const authRoutes = require('./auth.routes');
-const auth = require('../middlewares/auth');
 require('dotenv').config();
 
 console.log('Vars:', process.env.DB_USER, process.env.DB_SERVER, process.env.DB_NAME, process.env.DB_PASSWORD, process.env.DB_PORT);
